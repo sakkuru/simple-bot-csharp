6d5e2f8a774bedfd1712ca03af95dbecae287d2d
sakkuru <sakkuru0323@gmail.com>
2017-06-01 18:21:53 +0900

    first commit

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -23,6 +23,7 @@ namespace Bot_Application1.Dialogs
             int length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
+            await context.PostAsync("こんにちは");
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
